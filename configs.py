1c10d1a35533ada02cc7afce7753a10babe00375
ShadowKing9 <88343817+ShadowKing9@users.noreply.github.com>
2022-05-06 22:57:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,20 +30,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @ShadowKing9o
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+👥 **Support Group:** [Linux Repositories](https://t.me/ShadowsArena)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+🎥 **Request Group:** [Movie Adda](https://t.me/MovieAddaOfShadow)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @ShadowKing9o
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+Donate Now =}@nikkuoo889@paytm (UPI iD)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
